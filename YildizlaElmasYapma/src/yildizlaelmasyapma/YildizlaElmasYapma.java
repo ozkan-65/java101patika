2a86da6860d7b12fb9f8513c815c14d55910f6fc
ozkan-65 <90394055+ozkan-65@users.noreply.github.com>
2023-05-09 20:39:35 +0300

    Add files via upload

---
+++ YildizlaElmasYapma/src/yildizlaelmasyapma/YildizlaElmasYapma.java
@@ -0,0 +1,33 @@
+
+package yildizlaelmasyapma;
+
+import java.util.Scanner;
+
+public class YildizlaElmasYapma {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz:");
+        int n = input.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i)+1; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i = n; i >=0  ; i--) {
+            for (int j = (n - i); j >0 ; j--) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i)+1; k >=1 ; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
