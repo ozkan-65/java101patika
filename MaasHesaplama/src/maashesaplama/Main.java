d1c1ceb0b5683ad66bc2739da39491575ccc9575
ozkan-65 <90394055+ozkan-65@users.noreply.github.com>
2023-05-12 23:09:30 +0300

    Add files via upload

---
+++ MaasHesaplama/src/maashesaplama/Main.java
@@ -0,0 +1,15 @@
+
+package maashesaplama;
+
+public class Main {
+     public static void main(String[] args) {
+        MaasHesaplama e1 = new MaasHesaplama("Alex",2000,45,1985);
+        MaasHesaplama e2 = new MaasHesaplama("Michael",2000,50,2000);
+        e1.printInfo();
+        e2.printInfo();
+
+
+
+
+    }
+}
